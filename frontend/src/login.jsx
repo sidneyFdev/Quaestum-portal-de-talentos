66919f69f7e4d9f6688f0c0a5b1a74a7cbf91bb8
sidneyFdev <sidney.figueiredo97.dev@outlook.com.br>
2025-05-13 20:17:13 -0300

    feat: adicionar variáveis de cores e aplicar nas páginas

--- frontend/src/login.jsx
+++ frontend/src/login.jsx
@@ -1,19 +1,22 @@
 import React from "react";
 
 // Component responsible for rendering the login page UI
+
 const Login = () => {
   // All Tailwind classes for styling components.
+
   const formContainer = "relative flex mt-10";
   const formInput =
-    "peer w-full border-b-1 border-[var(--basic-border-color)] py-3 focus:border-[var(--basic-focused-border-color)] outline-0";
+    "peer w-full border-b-1 border-(--basic-border-color) py-3 focus:border-(--basic-focused-border-color) outline-0";
   const formLabel =
-    "absolute origin-center select-none cursor-text opacity-40 bottom-full transition-all duration-200 peer-focus:bottom-full peer-placeholder-shown:bottom-1/4";
+    "absolute origin-center select-none cursor-text opacity-60 bottom-full transition-all duration-200 peer-focus:bottom-full peer-placeholder-shown:bottom-1/4";
 
   return (
     // Main container for Login Page. Only frontend is implemented for now. The buttons and links has no functions.
+
     <div class="container h-full flex flex-col items-center justify-center transition-all duration-200">
       <form
-        class="flex flex-col bg-[rgba(255,255,255,0.1)] pt-16 pb-10 px-10 rounded-2xl text-left min-w-[400px] h-fit"
+        class="flex flex-col bg-(--card-background-color) pt-16 pb-10 px-10 rounded-2xl text-left min-w-[400px] h-fit border-1 border-(--basic-border-color)"
         action=""
       >
         <h2 class="text-3xl text-center font-medium">Bem vindo candidato!</h2>
@@ -48,7 +51,7 @@ const Login = () => {
         </div>
 
         <button
-          class="w-full bg-blue-500 my-10 py-4 text-[20px] font-medium rounded-sm cursor-pointer transition-colors duration-300 hover:bg-blue-400 active:outline-0 active:scale-99"
+          class="w-full bg-(--action-button-color) my-10 py-4 text-[20px] font-medium rounded-sm cursor-pointer transition-colors duration-300 text-white hover:bg-(--active-action-button-color) active:outline-0 active:scale-99 active:text-blue-200"
           type="button"
         >
           Login
@@ -66,7 +69,7 @@ const Login = () => {
               Lembre-se de mim
             </label>
           </div>
-          <a class="text-sm text-blue-400 hover:underline decoration-1 hover:cursor-pointer hover:text-blue-300">
+          <a class="text-sm text-(--primary-highlight-color) hover:underline decoration-1 hover:cursor-pointer hover:text-(--secondary-highlight-color)">
             Esqueci a senha!
           </a>
         </div>
@@ -74,7 +77,7 @@ const Login = () => {
       <div class="mt-5">
         <p class="text-[14px]">
           Ainda não é cadastrado?{" "}
-          <a class="text-blue-400 hover:underline decoration-1 hover:cursor-pointer hover:text-blue-300">
+          <a class="text-sm text-(--primary-highlight-color) hover:underline decoration-1 hover:cursor-pointer hover:text-(--secondary-highlight-color)">
             Crie uma conta!
           </a>
         </p>
