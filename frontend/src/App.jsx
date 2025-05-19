da3b5601b1383e65e5bd9319e09c105df06fefde
sidneyFdev <sidney.figueiredo97.dev@outlook.com.br>
2025-05-19 04:05:52 -0300

    refactor: ajustes na estrutura para receber os componentes de Provider

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,45 +1,22 @@
-import React, {act, useState} from "react";
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  BrowserRouter,
-} from "react-router-dom";
-import Login from "./auth/login";
-import Register from "./auth/register";
-import Header from './components/layout/header';
-import SideBar from "./components/layout/sidebar";
-import LoginAdmin from "./admin/login";
+import React from "react";
+import { BrowserRouter as Router, BrowserRouter } from "react-router-dom";
+import Header from "./components/layout/header";
 import Footer from "./components/layout/footer";
-import ResetPassword from "./auth/resetpassword";
 import AppRoutes from "./router/routes";
 
 const App = () => {
-
-  const [activeSideBar, setActiveSideBar] = useState(false)
-
   return (
     <BrowserRouter>
-      <Header activeSideBar={activeSideBar} toggleActiveSideBar={() => setActiveSideBar((prev) => !prev)} />
+      <Header />
       <div className="flex h-full w-full overflow-hidden relative">
-            <div
-              className={`
-              transition-all duration-300 ease-in-out overflow-x-hidden absolute sm:relative sm:block h-full z-20 border-r-1 border-(--basic-border-color)
-              ${activeSideBar ? ' w-full opacity-100 translate-x-0 sm:w-64' : 'w-0 opacity-0 -translate-x-full'}
-              overflow-y-auto
-              `}
-            >
-              <SideBar />
-            </div>
-          
-            <main className="flex flex-col flex-1 overflow-y-auto">
-              <div className="flex-1">
-                <AppRoutes />
-              </div>
+        <main className="flex flex-col flex-1 overflow-y-auto">
+          <div className="flex-1">
+            <AppRoutes />
+          </div>
 
-              <Footer />
-            </main>
-        </div>
+          <Footer />
+        </main>
+      </div>
     </BrowserRouter>
   );
 };
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,11 +1,33 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import './styles/global.css'
+import React, { useContext } from "react";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import "./styles/global.css";
+import { AuthContext, AuthProvider } from "./provider/auth_provider.jsx";
+import { RecruiterProvider } from "./provider/recruiter_provider.jsx";
+import "@fortawesome/fontawesome-free/css/all.min.css";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+const AppWithProviders = () => {
+  const { isAdmin, loading } = useContext(AuthContext);
+
+  if (loading) return <div>Carregando...</div>;
+
+  return isAdmin ? (
+    <RecruiterProvider>
+      <App />
+    </RecruiterProvider>
+  ) : (
     <App />
-  </StrictMode>,
-)
+  );
+};
+
+const Root = () => (
+  <StrictMode>
+    <AuthProvider>
+      <AppWithProviders />
+    </AuthProvider>
+  </StrictMode>
+);
+
+createRoot(document.getElementById("root")).render(<Root />);
